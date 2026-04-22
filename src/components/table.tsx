4b2f040ad8f1ab9fba09eeba2d9686d2769fed23
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:13:30 -0300

    Update table.tsx

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,1 +1,25 @@
+type TableProps = {
+  children: React.ReactNode;
+};
 
+export default function Table({ children }: TableProps) {
+  return (
+    <div
+      style={{
+        overflowX: "auto",
+        background: "#fff",
+        border: "1px solid #e5e7eb",
+        borderRadius: "12px",
+      }}
+    >
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse",
+        }}
+      >
+        {children}
+      </table>
+    </div>
+  );
+}
