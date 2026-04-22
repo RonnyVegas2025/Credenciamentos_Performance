3cb4a679d15b7d8e4a0b05eae429e2ff2aa93e16
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:14:17 -0300

    Update empresas.service.ts

--- src/services/empresas.service.ts
+++ src/services/empresas.service.ts
@@ -1,1 +1,8 @@
+import { supabase } from "@/lib/supabaseClient";
 
+export async function listarEmpresas() {
+  return await supabase
+    .from("empresas_clientes")
+    .select("*")
+    .order("created_at", { ascending: false });
+}
