76b56228c888e2da8df7a1d8f78b885b3ea9a851
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:14:06 -0300

    Update credenciamentos.service.ts

--- src/services/credenciamentos.service.ts
+++ src/services/credenciamentos.service.ts
@@ -1,1 +1,8 @@
+import { supabase } from "@/lib/supabaseClient";
 
+export async function listarCredenciamentos() {
+  return await supabase
+    .from("credenciamentos")
+    .select("*")
+    .order("data_cadastro", { ascending: false });
+}
