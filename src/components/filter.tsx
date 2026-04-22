43cfeb9c65867e3ea36558192bda1644996d38a4
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:13:05 -0300

    Update filter.tsx

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,1 +1,13 @@
+type FilterProps = {
+  label: string;
+  children: React.ReactNode;
+};
 
+export default function Filter({ label, children }: FilterProps) {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "6px" }}>
+      <label style={{ fontWeight: 600 }}>{label}</label>
+      {children}
+    </div>
+  );
+}
