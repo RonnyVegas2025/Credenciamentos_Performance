1ac8eef2cc8c76569cb62409d0ed641af2d11b39
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:14:42 -0300

    Update movimentacoes.service.ts

--- src/services/movimentacoes.service.ts
+++ src/services/movimentacoes.service.ts
@@ -1,1 +1,8 @@
+import { supabase } from "@/lib/supabaseClient";
 
+export async function listarMovimentacoes() {
+  return await supabase
+    .from("movimentacoes")
+    .select("*")
+    .order("data_movimentacao", { ascending: false });
+}
