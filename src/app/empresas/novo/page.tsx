75ac8b781c429b6a2d294d40acced4b4e28517ad
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 12:10:54 -0300

    Update page.tsx

--- src/app/empresas/novo/page.tsx
+++ src/app/empresas/novo/page.tsx
@@ -1,1 +1,8 @@
-
+export default function NovaEmpresaPage() {
+  return (
+    <main style={{ padding: "24px" }}>
+      <h1>Nova Empresa Cliente</h1>
+      <p>Aqui vamos montar o formulário de cadastro.</p>
+    </main>
+  );
+}
