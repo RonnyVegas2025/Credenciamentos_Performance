74b942e0860bccc42669f52543f078ea5949cb36
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-22 15:14:54 -0300

    Update database.types.ts

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,1 +1,29 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
 
+export type Database = {
+  public: {
+    Tables: {
+      empresas_clientes: {
+        Row: Record<string, unknown>;
+      };
+      solicitacoes_credenciamento: {
+        Row: Record<string, unknown>;
+      };
+      credenciamentos: {
+        Row: Record<string, unknown>;
+      };
+      credenciamentos_importados: {
+        Row: Record<string, unknown>;
+      };
+      movimentacoes: {
+        Row: Record<string, unknown>;
+      };
+    };
+  };
+};
